7963135dc8665e84d92cf5ba6c1095197c4e2b77
Papa-Sean <smsugrue@gmail.com>
2021-03-01 16:47:34 -0500

    well

--- qbclient/src/Server/index.js
+++ qbclient/src/Server/index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:" + process.env.PORT,
+      origin: "http://localhost:" + '*',
       methods: ["GET", "POST"]
     }
   });
--- qbserver/index.js
+++ qbserver/index.js
@@ -23,7 +23,7 @@ app.get('/', (req,res) =>{
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", '*');
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
