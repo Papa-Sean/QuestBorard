959b97aed846e634af9d7840451b91c8447ed432
Papa-Sean <smsugrue@gmail.com>
2021-03-01 20:02:58 -0500

    yep:

--- qbclient/src/Server/index.js
+++ qbclient/src/Server/index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-      origin: "localhost:" + '*',
+      origin: "https://dnd-chat-v2.herokuapp.com/",
       methods: ["GET", "POST"]
     }
   });
